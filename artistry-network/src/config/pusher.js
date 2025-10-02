2b6e6d8fb318772b621e80442882e59500f5933d
quangvu <vuff12345@gmail.com>
2025-10-03 00:09:59 +0700

    messager

--- artistry-network/src/config/pusher.js
+++ artistry-network/src/config/pusher.js
@@ -1,6 +1,7 @@
 const Pusher = require('pusher');
 require('dotenv').config();
 
+// Cấu hình Pusher
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_KEY,
